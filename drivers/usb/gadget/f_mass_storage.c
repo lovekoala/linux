zhanglimin/linux-sunxi

usb: gadget: Puke sun4i support from AllWinner's 2.6.36 tree

drivers/usb/gadget/f_mass_storage.c
@@ -3057,6 +3057,7 @@ static int fsg_bind_config(struct usb_composite_dev *cdev,
 	fsg->function.disable     = fsg_disable;
 
 	fsg->common               = common;
+    fsg->common->fsg          = fsg;
 	/* Our caller holds a reference to common structure so we
 	 * don't have to be worry about it being freed until we return
 	 * from this function.  So instead of incrementing counter now

lovekoala/linux

drivers/usb/gadget/f_mass_storage.c
@@ -3061,6 +3061,7 @@ static int fsg_bind_config(struct usb_composite_dev *cdev,
 	fsg->function.disable     = fsg_disable;
 
 	fsg->common               = common;
+	fsg->common->fsk	  = fsg;
 	/*
 	 * Our caller holds a reference to common structure so we
 	 * don't have to be worry about it being freed until we return